4d481e5fa1af31044c0dd3a22a090a7d13cf0bbc
DushanMalind <dushanmalinda010@gmail.com>
2023-02-25 15:26:22 +0530

    CustomerRepository

--- hiberante/src/lk/ijse/hiberanate/repository/CustomerRepository.java
+++ hiberante/src/lk/ijse/hiberanate/repository/CustomerRepository.java
@@ -1,4 +1,25 @@
 package lk.ijse.hiberanate.repository;
 
+import lk.ijse.hiberanate.entity.Customer;
+import lk.ijse.hiberanate.util.SessionFactoryConfiguaration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.Serializable;
+
 public class CustomerRepository {
+    
+    private final Session session;
+    
+    public CustomerRepository(){
+        session= SessionFactoryConfiguaration.getInstance().getSession();
+    }
+    
+    public Integer saveCustomer(Customer customer){
+        Transaction transaction = session.beginTransaction();
+        Integer id = (Integer)session.save(customer);
+        transaction.commit();
+
+        return id;
+    }
 }
